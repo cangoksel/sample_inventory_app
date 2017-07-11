b15d57f901c16aa48eb920efba0778c78712df64
gcan <gcan@innova.com.tr>
2017-07-11 14:36:24 +0300

    düzeltme

--- web/tools/config/seed.config.ts
+++ web/tools/config/seed.config.ts
@@ -384,7 +384,7 @@ export class SeedConfig {
       [this.BOOTSTRAP_MODULE]: `${this.APP_BASE}${this.BOOTSTRAP_MODULE}`,
       '@angular/animations': 'node_modules/@angular/animations/bundles/animations.umd.js',
       '@angular/platform-browser/animations': 'node_modules/@angular/platform-browser/bundles/platform-browser-animations.umd.js',
-      '@angular/common': 'node_modules/@angular/com.github.cangoksel.common/bundles/com.github.cangoksel.common.umd.js',
+      '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
       '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
       '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
@@ -394,7 +394,7 @@ export class SeedConfig {
       '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
       '@angular/animations/browser': 'node_modules/@angular/animations/bundles/animations-browser.umd.js',
 
-      '@angular/common/testing': 'node_modules/@angular/com.github.cangoksel.common/bundles/com.github.cangoksel.common-testing.umd.js',
+      '@angular/common/testing': 'node_modules/@angular/common/bundles/common-testing.umd.js',
       '@angular/compiler/testing': 'node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
       '@angular/core/testing': 'node_modules/@angular/core/bundles/core-testing.umd.js',
       '@angular/http/testing': 'node_modules/@angular/http/bundles/http-testing.umd.js',
@@ -454,7 +454,7 @@ export class SeedConfig {
         defaultExtension: 'js'
       },
       '@angular/common': {
-        main: 'bundles/com.github.cangoksel.common.umd.js',
+        main: 'bundles/common.umd.js',
         defaultExtension: 'js'
       },
       '@angular/compiler': {
